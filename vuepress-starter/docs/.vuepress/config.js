359ba1826e88f9715ced8c9bd054217556b3f136
GitHub用户 <user@example.com>
2025-08-20 06:15:22 +0800

    1

--- vuepress-starter/docs/.vuepress/config.js
+++ vuepress-starter/docs/.vuepress/config.js
@@ -1,6 +1,8 @@
 module.exports = {
   title: 'VuePress 文档',
   description: '我的第一个 VuePress 网站',
+  // 输出目录设置为根目录的docs文件夹，与GitHub Pages配置匹配
+  dest: '../../../docs',
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },
